ba283b3d892f962e26de311479b08af85f915170
DanielMora <moraarce18@gmail.com>
2026-05-18 22:26:13 -0500

    Initial commit

---
+++ src/main/java/com/novaclinic/novaclinic_backend/NovaclinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novaclinic.novaclinic_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaclinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaclinicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novaclinic/novaclinic_backend/NovaclinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaclinic.novaclinic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaclinicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
